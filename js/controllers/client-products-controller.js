a7217f35283656f998bb3e0b39cfe45dcb7a5b39
sam-rsuarez10 <suarezzcode2001@gmail.com>
2022-08-09 12:01:42 -0400

    Adición de la página de los productos para el cliente

--- js/controllers/client-products-controller.js
+++ js/controllers/client-products-controller.js
@@ -2,17 +2,6 @@ import { productServices } from "../services/product-service.js";
 
 const createListElement = (id, url, nombre, precio, type) => {
     const element = document.createElement("li");
-    // Creación de los íconos
-    const trash_icon = document.createElement("i");
-    trash_icon.classList.add("bx", "bxs-trash-alt", "bx-tada-hover");
-    trash_icon.setAttribute("id", id); // se le coloca el id del producto al ícono de basura correspondiente
-    
-    const link_icon = document.createElement("a");
-    link_icon.href = `./update-product.html?id=${id}`;
-    const pencil_icon = document.createElement("i");
-    pencil_icon.classList.add("bx", "bxs-pencil", "bx-tada-hover");
-    link_icon.appendChild(pencil_icon);
-
     // Creación del div que contendrá la imagen
     const divImage = document.createElement("div");
     if(type == "default"){
@@ -24,14 +13,6 @@ const createListElement = (id, url, nombre, precio, type) => {
         divImage.style.background = `url(${url}) no-repeat center / cover`;
     }
     divImage.classList.add("producto__imagen");
-    // Creación del div que contendrá los íconos
-    const divIcon = document.createElement("div");
-    divIcon.classList.add("icon__container");
-    // Inserción de los iconos al div de íconos
-    divIcon.appendChild(trash_icon);
-    divIcon.appendChild(link_icon);
-    // Inserción del div de íconos al div de la imagen
-    divImage.appendChild(divIcon);
     element.appendChild(divImage);
 
     // Creación del h4 para el nombre del producto
@@ -46,13 +27,6 @@ const createListElement = (id, url, nombre, precio, type) => {
     p_precio.classList.add("producto__precio");
     element.appendChild(p_precio);
     element.classList.add("productos__item");
-
-    trash_icon.addEventListener("click", (event) => {
-        event.preventDefault();
-        const id_trash = trash_icon.id;
-        productServices.deleteProduct(id_trash).then(() => console.log("Eliminado"))
-        .catch(() => alert("No se pudo eliminar el producto, intente más tarde"));
-    });
     return element;
 }
 
